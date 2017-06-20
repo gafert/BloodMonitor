30ef72146f5167565eff55d1f9a05bcfa72b83c4
gafert <m.gafert@outlook.com>
2017-06-20 16:40:55 +0200

    Added chart element limit

--- app/src/main/java/fhtw/bsa2/gafert_steiner/BloodMonitor/activities/MainActivity.java
+++ app/src/main/java/fhtw/bsa2/gafert_steiner/BloodMonitor/activities/MainActivity.java
@@ -3,6 +3,7 @@ package fhtw.bsa2.gafert_steiner.BloodMonitor.activities;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +13,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -70,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final AppBarLayout appBar = (AppBarLayout) findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Chart");
 
@@ -117,11 +120,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged() {
                 itemArrayAdapter.replaceAll(ItemHolder.getInstance().getItems());
+                recyclerView.scrollToPosition(0);
                 setupGraph(ItemHolder.getInstance().getItems());
             }
         });
 
         final EditText searchEditText = (EditText) findViewById(R.id.searchEditText);
+        searchEditText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                appBar.setExpanded(false);
+                return false;
+            }
+        });
         searchEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -168,59 +179,62 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void setupGraph(final ArrayList<Item> data) {
+    private void setupGraph(final List<Item> _data) {
 
         LineChart chart = (LineChart) findViewById(R.id.chart);
+        int crop = 7;
+        ArrayList<Item> data = new ArrayList<>();
+        if (_data.size() > crop) {
+            data = new ArrayList<>(_data.subList(_data.size() - crop, _data.size()));
+        } else {
+            data.addAll(_data);
+        }
 
-        if (data != null) {
-            if (!data.isEmpty()) {
-                // Entry Array
-                List<Entry> happinessEntries = new ArrayList<>();
-                int count = 0;
-                for (Item entry : data) {
-                    happinessEntries.add(new Entry(count, entry.getMood()));
-                    count++;
-                }
-
-                // Das die werte nicht auf der seite kleben
-                chart.getXAxis().setAxisMinimum(-1.2f);
-                chart.getXAxis().setAxisMaximum(count + 0.2f);
-
-                // Colors for styling
-                int[] colors = new int[3];
-                colors[0] = ContextCompat.getColor(this, R.color.colorAccent);
-                colors[1] = ContextCompat.getColor(this, R.color.colorPrimary);
-
-                // Make a new data set with entries
-                LineDataSet happinessDateSet = new LineDataSet(happinessEntries, "Happiness");
-
-                // Style the dataSet
-                happinessDateSet.setColor(colors[0]);                       // Format Line
-                happinessDateSet.setCircleColor(colors[0]);
-                happinessDateSet.setCircleColorHole(colors[1]);
-                happinessDateSet.setCircleRadius(7);
-                happinessDateSet.setCircleHoleRadius(5);
-                happinessDateSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);    // Makes it line smooth
-                happinessDateSet.setHighlightEnabled(true);                 // Allow highlighting for DataSet
-                happinessDateSet.setDrawHighlightIndicators(false);         // Draw point on which someone clicked
-                happinessDateSet.setLineWidth(2.5f);
-
-                List<ILineDataSet> dataSet = new ArrayList<ILineDataSet>();
-                dataSet.add(happinessDateSet);                              // All lines are added to a dataSet
-
-                LineData lineData = new LineData(dataSet);
-                lineData.setDrawValues(false);                              // Disable point labeling
-
-                chart.setData(lineData);
-                chart.moveViewTo(chart.getData().getEntryCount(), 0, YAxis.AxisDependency.RIGHT);   // Set viewport to last entries
-            } else {
-                chart.clear();
-                chart.invalidate();
+        if (!data.isEmpty()) {
+            // Entry Array
+            List<Entry> happinessEntries = new ArrayList<>();
+            int count = 0;
+            for (Item entry : data) {
+                happinessEntries.add(new Entry(count, entry.getMood()));
+                count++;
             }
+
+            // Das die werte nicht auf der seite kleben
+            chart.getXAxis().setAxisMinimum(-1.2f);
+            chart.getXAxis().setAxisMaximum(count + 0.2f);
+
+            // Colors for styling
+            int[] colors = new int[3];
+            colors[0] = ContextCompat.getColor(this, R.color.colorAccent);
+            colors[1] = ContextCompat.getColor(this, R.color.colorPrimary);
+
+            // Make a new data set with entries
+            LineDataSet happinessDateSet = new LineDataSet(happinessEntries, "Happiness");
+
+            // Style the dataSet
+            happinessDateSet.setColor(colors[0]);                       // Format Line
+            happinessDateSet.setCircleColor(colors[0]);
+            happinessDateSet.setCircleColorHole(colors[1]);
+            happinessDateSet.setCircleRadius(7);
+            happinessDateSet.setCircleHoleRadius(5);
+            happinessDateSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);    // Makes it line smooth
+            happinessDateSet.setHighlightEnabled(true);                 // Allow highlighting for DataSet
+            happinessDateSet.setDrawHighlightIndicators(false);         // Draw point on which someone clicked
+            happinessDateSet.setLineWidth(2.5f);
+
+            List<ILineDataSet> dataSet = new ArrayList<ILineDataSet>();
+            dataSet.add(happinessDateSet);                              // All lines are added to a dataSet
+
+            LineData lineData = new LineData(dataSet);
+            lineData.setDrawValues(false);                              // Disable point labeling
+
+            chart.setData(lineData);
+            chart.moveViewTo(chart.getData().getEntryCount(), 0, YAxis.AxisDependency.RIGHT);   // Set viewport to last entries
         } else {
             chart.clear();
             chart.invalidate();
         }
+
 
         // Add the lines to the chart
         chart.getLegend().setEnabled(false);                                // Disables Legend
@@ -246,7 +260,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Always draw Y as high as max values + offset
         chart.getAxisLeft().setAxisMaximum(FEELING_VERY_HAPPY + 5);
-        chart.getAxisLeft().setAxisMinimum(FEELING_VERY_SAD - 3);
+        chart.getAxisLeft().setAxisMinimum(FEELING_VERY_SAD - 5);
 
         // Disable all Y Axis except 1
         chart.getAxisLeft().setDrawAxisLine(false);
